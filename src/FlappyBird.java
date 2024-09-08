2f10b513177f0964cc14d16d9f53cd364546d7dc
kazimoto5520 <meshackkazimoto@gmail.com>
2024-09-08 20:26:56 +0300

    add pipe timer

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class FlappyBird extends JPanel implements ActionListener, KeyListener{
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -27,18 +27,42 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         int height = birdHeight;
         Image img;
 
-        Bird(Image img){
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    // Pipes
+    int pipeX = boardWidth;
+    int pipeY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+
+    class Pipe {
+        int x = pipeX;
+        int y = pipeY;
+        int width = pipeWidth;
+        int height = pipeHeight;
+        Image img;
+        boolean passed = false;
+
+        Pipe(Image img) {
             this.img = img;
         }
     }
 
     // Game Logic
     Bird bird;
+    int velocityX = -4;
     int velocityY = 0;
     int gravity = 1;
-    Timer gameLoop;
 
-    FlappyBird(){
+    ArrayList<Pipe> pipes;
+
+    Timer gameLoop;
+    Timer placePipeTimer;
+
+    FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         // setBackground(Color.BLUE);
         setFocusable(true);
@@ -48,59 +72,85 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
         birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
         topPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage(); 
+        bottomPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
 
         // Bird
         bird = new Bird(birdImage);
+        pipes = new ArrayList<Pipe>();
+
+        // place pipe timer
+        placePipeTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+
+        placePipeTimer.start();
 
         // Timer
-        gameLoop = new Timer(1000/60, this);
+        gameLoop = new Timer(1000 / 60, this);
         gameLoop.start();
     }
 
-    public void paintComponent(Graphics g){
+    public void placePipes() {
+
+        Pipe topPipe = new Pipe(topPipeImage);
+        pipes.add(topPipe);
+        
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
 
     }
 
-    public void draw(Graphics g){
+    public void draw(Graphics g) {
         // System.out.println("draw");
         g.drawImage(backgroundImage, 0, 0, boardWidth, boardHeight, null);
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
         g.drawImage(topPipeImage, 50, 50, 50, 50, null);
         g.drawImage(bottomPipeImage, 50, 50, 50, 50, null);
+
+        for(int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
     }
 
-    public void move(){
+    public void move() {
         velocityY += gravity;
         bird.y += velocityY;
         bird.y = Math.max(bird.y, 0);
+
+        for(int i = 0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX;
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
-       repaint();
+        repaint();
     }
 
-    
     @Override
     public void keyPressed(KeyEvent e) {
-    
+
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-       if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-              velocityY = -9;
-       }
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            velocityY = -9;
+        }
     }
-
 
     @Override
     public void keyReleased(KeyEvent e) {
-        
+
     }
-    
+
 }
